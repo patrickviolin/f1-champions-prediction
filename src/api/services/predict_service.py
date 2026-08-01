5a59b3fdb327dfbaaa67913965fd49d1ad908309
patrick-jk <patrick.pviolin@senacsp.edu.br>
2026-08-01 18:39:13 -0300

    add: service for backend use of ML

--- src/api/services/predict_service.py
+++ src/api/services/predict_service.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 from xgboost import XGBRanker, XGBRegressor
 
@@ -6,15 +8,21 @@ from api.schemas.predict_dto import RacePredictionRequest, RacePredictionRespons
 
 class PredictService:
     def __init__(self):
-        self.regressor_model = XGBRanker()
-        self.ranker_model = XGBRegressor()
+        project_root = Path(__file__).parent.parent.parent.parent
 
-        self.ranker_model.load_model('../../../models/xgb_ranker_model.json')
-        self.regressor_model.load_model('../../../models/xgb_regressor_model.json')
+        ranker_path = project_root / 'models' / 'xgb_ranker_model.json'
+        regressor_path = project_root / 'models' / 'xgb_regressor_model.json'
+
+        self.regressor_model = XGBRegressor()
+        self.regressor_model.load_model(regressor_path)
+
+        self.ranker_model = XGBRanker()
+        self.ranker_model.load_model(ranker_path)
 
     def execute_prediction(self, request: RacePredictionRequest, model_type: str) -> RacePredictionResponse:
         """
         Execute prediction request: Converts the payload into DataFrame, predicts the score and returns the prediction result.
+        :param model_type: regressor or ranker
         :param request:RacePredictionRequest
         :return:RacePredictionResponse
         """
@@ -24,12 +32,24 @@ class PredictService:
         driver_names = df_input['driver_ref'].copy()
         df_features = df_input.drop(columns='driver_ref')
 
-        categorical_cols = ['circuitId']
+        df_features = df_features.rename(columns={'constructor_id': 'constructorId', 'circuit_id': 'circuitId'})
+
+        categorical_cols = ['constructorId', 'circuitId']
         for col in categorical_cols:
             if col in df_features.columns:
                 df_features[col] = df_features[col].astype('category')
 
-        if model_type == 'ranker':
+        expected_order = [
+            'driver_age', 'driver_momentum', 'position_qualifying', 'q1_millis',
+            'reached_q1', 'q2_millis', 'reached_q2', 'q3_millis', 'reached_q3',
+            'constructorId', 'constructor_momentum', 'round', 'circuitId', 'grid',
+            'driver_track_affinity', 'constructor_track_affinity', 'constructor_dnf_rate',
+            'driver_dnf_rate', 'circuit_dnf_rate'
+        ]
+
+        df_features = df_features[expected_order]
+
+        if model_type.lower() == 'ranker':
             predictions = self.ranker_model.predict(df_features)
         else:
             predictions = self.regressor_model.predict(df_features)
