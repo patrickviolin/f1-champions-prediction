05821407e075c60a413baec954fad65840c46146
patrick-jk <patrick.pviolin@senacsp.edu.br>
2026-08-01 11:03:23 -0300

    refactor: pythonic structure for DTOs

--- src/api/schemas/driver_prediction.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class DriverPrediction(BaseModel):
-    """Represents the isolated driver prediction"""
-    driver_ref: str
-    predicted_score: float
-    predicted_position: int
